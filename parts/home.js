b8f0f6c3b1f195e3cc7d36861ef25e497f4cd118
arianestolfi <arianestolfi@gmail.com>
2020-09-03 23:45:19 -0300

    creating angular structure

---
+++ parts/home.js
@@ -0,0 +1,69 @@
+app.controller('mapaController', ['$scope', '$window', '$http', '$location', '$filter', function ($scope, $window, $http, $location, $filter) {
+
+
+    //carrega objetos    
+
+    var autores = [];
+
+ 
+    $scope.buscaobj = 'scripts/services/files.json';
+
+
+
+    $http({
+        method: 'GET',
+        url: $scope.buscaobj
+    }).then(function successCallback(response) {
+        //console.log(response.data);
+        $scope.objetos = response.data;
+        $scope.arrobj = $filter('array')($scope.objetos);
+        $scope.arrobj = $filter('orderBy')($scope.arrobj, 'idno');
+
+        return $scope.arrobj;
+        //console.log($scope.arrobj);
+
+    }, function errorCallback(response) {
+
+
+        // or server returns response with an error status.
+
+
+    }, function errorCall(response) {
+
+    });
+
+
+
+    });
+
+
+
+
+
+
+    var query = $location.path();
+
+    //observa mudanças no endereço
+
+
+
+
+    //end of controller
+}]);
+
+
+
+
+
+
+
+
+
+//pagina par
+// move esquerda
+// move proxima
+
+
+//pagina impar
+// move direita
+// move proxima
--- scripts/controllers/app.js
+++ scripts/controllers/app.js
@@ -4,6 +4,80 @@ var app = angular.module('app', [
 
 ]);
 
-app.controller("hermes", function($scope){
-	
-})
+function addLeadingZeros (n, length)
+{
+    var str = (n > 0 ? n : -n) + "";
+    var zeros = "";
+    for (var i = length - str.length; i > 0; i--)
+        zeros += "0";
+    zeros += str;
+    return n >= 0 ? zeros : "-" + zeros;
+}
+
+
+
+
+
+//testController end  
+
+
+
+app.config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    //$locationProvider.html5Mode(true);
+    $routeProvider
+      .
+    when('/admin', {
+            templateUrl: 'parts/home.html',
+            controller: 'homeController'
+        }).otherwise({
+        
+        templateUrl: 'parts/home.html',
+            controller: 'homeController'
+    });
+    
+            //$locationProvider.html5Mode(true);
+
+    
+$httpProvider.defaults.useXDomain = true;
+$httpProvider.defaults.withCredentials = true;
+delete $httpProvider.defaults.headers.common["X-Requested-With"];
+$httpProvider.defaults.headers.common["Accept"] = "application/json";
+$httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+    
+  });
+
+
+
+app.filter('numberFixedLen', function () {
+    return function (a, b) {
+        return (1e4 + a + "").slice(-b)
+    }
+});
+
+app.filter('numberStr', function () {
+    return function (string) {
+        parseInt(number);
+    }
+});
+
+app.filter('rawHtml', ['$sce', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
+}]);
+
+
+
+app.filter('array', function() {
+  return function(items) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+   return filtered;
+  };
+});
+
+
+
