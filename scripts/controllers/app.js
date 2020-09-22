f324442221079f46e03282b682e551bcd32204da
chgp <chgp@riseup.net>
2020-09-21 22:42:23 -0300

    htaccess, more lib fixes, added admin.js copied from home.js

--- scripts/controllers/app.js
+++ scripts/controllers/app.js
@@ -1,32 +1,18 @@
 var app = angular.module('app', [
     'ngRoute',
     'ngTouch'
-
 ]);
 
-function addLeadingZeros (n, length)
-{
-    var str = (n > 0 ? n : -n) + "";
-    var zeros = "";
-    for (var i = length - str.length; i > 0; i--)
-        zeros += "0";
-    zeros += str;
-    return n >= 0 ? zeros : "-" + zeros;
-}
 
-//testController end
 app.config(function ($routeProvider, $locationProvider, $httpProvider) {
-
     //$locationProvider.html5Mode(true);
-    $routeProvider
-        .
-        when('/admin', {
-            templateUrl: 'parts/admin.html',
-            controller: 'hermesController'
-        }).otherwise({
-            templateUrl: 'parts/home.html',
-            controller: 'hermesController'
-        });
+    $routeProvider.when('/admin', {
+        templateUrl: 'parts/admin.html',
+        controller: 'hermesController'
+    }).otherwise({
+        templateUrl: 'parts/home.html',
+        controller: 'hermesController'
+    });
 
     //$locationProvider.html5Mode(true);
 
@@ -54,7 +40,6 @@ app.filter('rawHtml', ['$sce', function($sce){
     return $sce.trustAsHtml(val);
   };
 }]);
-
 
 app.filter('array', function() {
   return function(items) {
